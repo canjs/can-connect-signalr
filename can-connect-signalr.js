64f6b175b117f4eaa25eafe403266f5cbe9c3194
Joe Crick <joe@bitovi.com>
2016-12-02 21:38:44 +0100

    (master): add init and getList tests  (master): Add create and update tests  (master): Add id property to hubConnection

--- can-connect-signalr.js
+++ can-connect-signalr.js
@@ -1,35 +1,27 @@
-var connect = require("can-connect"),
-	$ = require("jquery");
+var connect = require("can-connect");
+var $ = require("jquery");
 
 require('ms-signalr-client');
 
-module.exports = connect.behavior('signal-r', function signalR(baseConnection) {
+module.exports = connect.behavior('can-connect-signalr', function signalR(baseConnection) {
 	return {
 		init: function () {
 			baseConnection.init.apply(this, arguments);
 			var context = this;
 			this.signalR.ready = new Promise(function (resolve, reject) {
 				var signalR = context.signalR;
+				var name = signalR.name.toLowerCase();
 
-				// There is no official naming standard for SignalR Hub methods that extends beyond
-				// Hub names being PascalCased (server side and client side),a Hub names ending in
-				// "Hub"
 				signalR.connection = $.hubConnection(signalR.url);
 				signalR.proxy = signalR.connection.createHubProxy(signalR.name + 'Hub');
 
-				// SignalR convention is for PascalCased methods on the server to be
-				// camelCased on the client. There are no naming conventions for actions
-				// defined on the server side. However, we have implemented CUD methods
-				// using the following standards.
-				var name = signalR.name.toLowerCase();
-
-				signalR.proxy.on(name + "Created", function (item) {
+				signalR.proxy.on((signalR.createdName || name + "Created"), function (item) {
 					context.createInstance(item);
 				});
-				signalR.proxy.on(name + "Updated", function (item) {
+				signalR.proxy.on((signalR.updatedName || name + "Updated"), function (item) {
 					context.updateInstance(item);
 				});
-				signalR.proxy.on(name + "Destroyed", function (item) {
+				signalR.proxy.on((signalR.destroyedName || name + "Destroyed"), function (item) {
 					context.destroyInstance(item);
 				});
 
@@ -43,23 +35,22 @@ module.exports = connect.behavior('signal-r', function signalR(baseConnection) {
 		},
 		createData: function (props) {
 			return this.signalR.ready.then(function (signalR) {
-				return signalR.proxy.invoke(signalR.createName || ( signalR.name.toLowerCase() + "Create"), props);
+				return signalR.proxy.invoke(signalR.createName || (signalR.name.toLowerCase() + "Create"), props);
 			});
 		},
 		updateData: function (props) {
 			return this.signalR.ready.then(function (signalR) {
-				return signalR.proxy.invoke(signalR.name.toLowerCase() + "Update", props);
+				return signalR.proxy.invoke(signalR.updateName || (signalR.name.toLowerCase() + "Update"), props);
 			});
 		},
 		destroyData: function (props) {
 			return this.signalR.ready.then(function (signalR) {
-				return signalR.proxy.invoke(signalR.name.toLowerCase() + "Destroy", props);
+				return signalR.proxy.invoke(signalR.destroyName || (signalR.name.toLowerCase() + "Destroy"), props);
 			});
 		},
 		getListData: function (set) {
 			return this.signalR.ready.then(function (signalR) {
-				// We don't persist any data on the server, so we return a blank set on init.
-				return signalR.proxy.invoke(signalR.name.toLowerCase() + "GetList", set);
+				return signalR.proxy.invoke(signalR.getListName || (signalR.name.toLowerCase() + "GetList"), set);
 			});
 		}
 	};
--- test.js
+++ test.js
@@ -1,9 +1,167 @@
-var QUnit = require("steal-qunit");
+var QUnit = require('steal-qunit');
+var clone = require('steal-clone');
+var connect = require('can-connect');
+var dataParse = require('can-connect/data/parse/');
+var constructor = require('can-connect/constructor/');
+var constructorStore = require('can-connect/constructor/store/');
+var canMap = require('can-connect/can/map/');
+var dataCallbacks = require('can-connect/data/callbacks/');
+var realTime = require('can-connect/real-time/');
+var constructorCallbacksOnce = require('can-connect/constructor/callbacks-once/');
+var DefineMap = require('can-define/map/');
+var DefineList = require('can-define/list/');
 
-QUnit.module("can-connect-signalr",{
+var Message = DefineMap.extend({
+	text: {
+		type: 'string'
+	},
+	id: {
+		type: 'number'
+	}
+});
+
+Message.List = DefineList.extend({
+	Map: Message
+}, {});
+
+var testData = {"text": "this", "id": 1};
+
+QUnit.module('can-connect-signalr', {
+
+	beforeEach: function (assert) {
+		var done = assert.async();
+
+		clone({
+			'jquery': {
+				hubConnection: function () {
+					return {
+						id: 2228271782,
+						createHubProxy: function createHubProxy() {
+							return {
+								on: function on() {
+
+								},
+								invoke: function invoke(methodName) {
+									var promise;
+									switch(methodName) {
+										case "messageGetList":
+											promise = new Promise(function (resolve, reject) {
+												resolve([{
+													data: [testData]
+												}]);
+											});
+											break;
+										case "messageUpdate":
+											promise = new Promise(function (resolve, reject) {
+												testData.text = 'Hello!';
+												resolve(testData);
+											});
+											break;
+										default:
+											promise = new Promise(function (resolve, reject) {
+												resolve(testData);
+											});
+											break;
+									}
+
+									return promise;
+								}
+							};
+						},
+						start: function start() {
+							return {
+								done: function done(fn) {
+									fn();
+								}
+							}
+						}
+					};
+				}
+			},
+			'ms-signalr-client': {}
+		})
+			.import('./can-connect-signalr')
+			.then(function (signalR) {
+
+				var behaviors = [
+					dataParse,
+					constructor,
+					constructorStore,
+					canMap,
+					dataCallbacks,
+					realTime,
+					constructorCallbacksOnce,
+					signalR
+				];
+
+				Message.connection = connect(behaviors, {
+					Map: Message,
+					List: Message.List,
+					signalR: {
+						url: 'http://test.com',
+						name: 'Message'
+					}
+				});
+
+				done();
+
+			});
+	}
+});
+
+QUnit.test('Plugin initializes correctly', function () {
+	QUnit.equal(Message.connection.__behaviorName, 'can-connect-signalr');
+});
+
+QUnit.test('getList', function (assert) {
+	var done = assert.async();
+
+	Message.getList().then(function (messages) {
+		assert.ok(messages, 'Got a response from findAll');
+		assert.equal(messages instanceof Message.List, true, 'got a Message.List back');
+		done();
+	});
 
 });
 
-QUnit.test("basics of signalr", function(){
-    QUnit.ok(true, "passes");
+QUnit.test('create', function (assert) {
+	var done = assert.async();
+	var message = new Message({
+		text: 'Hi there!'
+	});
+
+	message.save().then(function (msg) {
+		assert.ok(msg);
+		done();
+	});
+});
+
+QUnit.test('update', function (assert) {
+	var done = assert.async();
+	var message = new Message({
+		text: 'Hi there!'
+	});
+
+	message.save().then(function (msg) {
+		msg.text = 'Hello!';
+		msg.save().then(function (saveResponse) {
+			assert.equal(saveResponse.text, 'Hello!', 'message text updated correctly');
+			done();
+		});
+	});
+});
+
+QUnit.test('destroy', function(assert){
+	var done = assert.async();
+
+	var message = new Message({
+		text: 'Hi there!'
+	});
+	message.save().then(function(msg){
+		var id = msg._id;
+		msg.destroy().then(function(res){
+			assert.equal(res._id, id, 'deleted the instance');
+			done();
+		});
+	});
 });
